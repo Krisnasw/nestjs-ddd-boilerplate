4a04844940e54f4ff3cca3ce1b365418aa7723d7
krisnasw <outattacker@gmail.com>
2023-08-19 20:49:41 +0700

    fix: update hello world controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,13 @@
 import { Controller, Get, HttpStatus, HttpCode } from '@nestjs/common';
-import { RedisOptions, Transport } from '@nestjs/microservices';
 import { ApiExcludeEndpoint, ApiResponse, ApiTags } from '@nestjs/swagger';
-import {
-  MicroserviceHealthIndicator,
-  HealthCheck,
-  HealthCheckService,
-  PrismaHealthIndicator,
-} from '@nestjs/terminus';
-import { PrismaClient } from '@prisma/client';
 
 import { AppService } from './app.service';
-import { SettingService } from './shared/services/setting.service';
 
 @Controller('/')
 @ApiTags('hello-world')
 export class AppController {
   constructor(
     private readonly _appService: AppService,
-    private health: HealthCheckService,
-    private readonly microservice: MicroserviceHealthIndicator,
-    private readonly db: PrismaHealthIndicator,
-    private readonly configService: SettingService,
   ) {}
 
   @Get('/')
@@ -29,29 +16,5 @@ export class AppController {
   @ApiResponse({ status: HttpStatus.OK, description: 'Hello world' })
   getHello(): string {
     return this._appService.getHello();
-  }
-
-  @Get('health-check')
-  @HealthCheck()
-  async healthCheck() {
-    const dbCheckResult = await this.db.pingCheck(
-      'database',
-      new PrismaClient(),
-    );
-    const redisCheckResult = await this.microservice.pingCheck<RedisOptions>(
-      'redis',
-      {
-        transport: Transport.REDIS,
-        options: {
-          host: this.configService.redis.host,
-          port: +this.configService.redis.port,
-          username: this.configService.redis.user,
-          password: this.configService.redis.password,
-          connectionName: this.configService.redis.connectionName,
-        },
-      },
-    );
-
-    return this.health.check([dbCheckResult, redisCheckResult]);
   }
 }
--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 
 import { UserService } from '@/modules/users/services/user.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCustomHeader } from '@/shared/swagger/decorator';
 
+@ApiTags('Users')
+@ApiCustomHeader()
 @Controller('users')
+// @UseInterceptors(ApiResponse)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
